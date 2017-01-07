75a6398658149e461cc7a64283c8de1ee4801230
Stephen Lewis <stephen@experiencehq.net>
2017-01-07 11:29:54 +0000

    Reinstate LaunchDaemon. Still required.

--- setup-docker.sh
+++ setup-docker.sh
@@ -22,5 +22,12 @@ brew install dinghy
 echo '- Cleaning up...'
 brew cleanup -s
 
+echo '- Configure loopback...'
+# Do the network dance, to get Docker and xdebug working.
+# @see https://gist.github.com/ralphschindler/535dc5916ccbd06f53c1b0ee5a868c93
+sudo curl -o \
+  /Library/LaunchDaemons/com.ralphschindler.docker_10254_alias.plist \
+  https://gist.githubusercontent.com/ralphschindler/535dc5916ccbd06f53c1b0ee5a868c93/raw/com.ralphschindler.docker_10254_alias.plist
+
 echo '- All done.'
 echo '--------------------'
