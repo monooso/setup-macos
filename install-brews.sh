52107af75dc230070e8b52a842e5062d23016465
Stephen Lewis <stephen@experiencehq.net>
2017-05-17 23:44:11 -0700

    Update Homebrew list.

--- install-brews.sh
+++ install-brews.sh
@@ -4,7 +4,12 @@ echo 'Installing tools...'
 echo '-------------------'
 brew install ansible
 brew install coreutils
+brew install global --with-pygments --with-sqlite3
 brew install git
+brew install python3
 brew install wget
+
+brew tap neovim/neovim
+brew install neovim
 
 brew cleanup -s
