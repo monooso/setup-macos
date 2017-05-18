319ff820c4a362af778c5b27f774a2ffb8b7837a
Stephen Lewis <stephen@experiencehq.net>
2017-05-18 00:05:43 -0700

    Use universal ctags. GNU global is a PITA.

--- install-brews.sh
+++ install-brews.sh
@@ -4,12 +4,14 @@ echo 'Installing tools...'
 echo '-------------------'
 brew install ansible
 brew install coreutils
-brew install global --with-pygments --with-sqlite3
 brew install git
 brew install python3
 brew install wget
 
 brew tap neovim/neovim
 brew install neovim
+
+brew tap universal-ctags/universal-ctags
+brew install --HEAD universal-ctags
 
 brew cleanup -s
