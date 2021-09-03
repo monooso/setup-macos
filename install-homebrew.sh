a0be0153ffd1cfafec0ea8ec0138565d82772c1b
Stephen Lewis <monooso@Supers-MacBook-Pro.local>
2021-09-03 19:13:55 +0100

    Fix Homebrew installation
    Add the local Homebrew binary directory to the PATH for subsequent scripts.

--- install-homebrew.sh
+++ install-homebrew.sh
@@ -2,5 +2,6 @@
 
 echo ''
 echo '⚡️ Installing Homebrew'
-mkdir homebrew
-curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+mkdir ~/homebrew
+export PATH="$HOME/homebrew/bin:$PATH"
+curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/homebrew
